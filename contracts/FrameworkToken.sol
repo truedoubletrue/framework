b3e228b915c8632bc2de178a8d846c80e9f03dbb
Jason Konicki <jasonkonicki@yahoo.com>
2018-04-27 04:34:53 -0400

    Updated token to ERC223 token, renamed crowdsale token

--- contracts/FrameworkToken.sol
+++ contracts/FrameworkToken.sol
@@ -3,14 +3,30 @@ pragma solidity ^0.4.19;
 import 'zeppelin-solidity/contracts/ownership/Contactable.sol';
 import 'zeppelin-solidity/contracts/lifecycle/Pausable.sol';
 
-import 'zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
 import './ERC223/ERC223Token.sol';
-import './MembershipAccessControl.sol';
+//import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 
-contract FrameworkToken is MintableToken, ERC223Token, /*MembershipAccessControl,*/ Pausable, Contactable {
+contract FrameworkToken is ERC223Token, Pausable, Contactable {
   string public name = "Framework";
   string public symbol = "FRWK";
   uint8 public decimals = 18;
+  //uint256 public cappedTokenSupply = 100000000 * (10 ** uint256(decimals)); // There will be total 100 million FRWK Tokens
   
+  /*function FrameworkToken(uint256 cappedTokenSupply) CappedERC223Token(cappedTokenSupply) public {
+  	
+  }
+
+  function transfer(address _to, uint256 _value) public whenNotPaused returns (bool _success) {
+    return super.transfer(_to, _value);
+  }
+
+  function transfer(address _to, uint256 _value, bytes _data, string _fallback) public whenNotPaused returns (bool _success) {
+  	return super.transfer(_to, _value, _data, _fallback);
+  }
+
+  function transfer(address _to, uint256 _value, bytes _data) public whenNotPaused returns (bool _success) {
+  	return super.transfer(_to, _value, _data);
+  }
+*/
 }
 
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,20 +1,21 @@
-var frameworkCrowdsale = artifacts.require("FrameworkCrowdsale");
+var frameworkInvest = artifacts.require("FrameworkInvest");
 var frameworkToken = artifacts.require("FrameworkToken");
 
 module.exports = function(deployer, network, accounts) {
-	const rate = new web3.BigNumber(5000);
+	const rate = new web3.BigNumber(500);
+    const cap = new web3.BigNumber(100000000000000000000000000);
 	const wallet = accounts[1];
 	
 	return deployer.then(() => {
-		return deployer.deploy(frameworkToken);
+		return deployer.deploy(frameworkToken, cap);
 	})
-		.then(() => {
+		/*.then(() => {
 			return deployer.deploy(			
-				frameworkCrowdsale,  
-				rate, 
+				frameworkInvest,  
+				rate, //500 tokens per ether
 				wallet,//"0xD43FC396fc0E7bcc0dDe7c9BCe1936d1b24B0f4a", // Replace this wallet address with the last one (10th account) from Ganache UI. This will be treated as the beneficiary address. 
-				500000000000000000000,// 500 ETH
+				cap,// 100 million token cap
 				frameworkToken.address
     		);
-		});
+		});*/
   };
